f16cb98996da73a48e35b8ac1d818b08526c0124
Sitt Guruvanich <aekazitt@gmail.com>
2023-10-04 01:13:00 +0700

    Define LND env vars

--- src/configs.py
+++ src/configs.py
@@ -15,8 +15,12 @@ Module detailing environment variables used by server and services
 
 ### Local modules ###
 from os import environ
+from typing import Optional
 
 DEFAULT_TIMEZONE: str = environ.get("DEFAULT_TIMEZONE", "Asia/Bangkok")
+LND_HOST_URL: str = environ.get("LND_HOST_URL", "localhost:10009")
+LND_MACAROON_PATH: Optional[str] = environ.get("LND_MACAROON_PATH", None)
+LND_TLSCERT_PATH: Optional[str] = environ.get("LND_TLSCERT_PATH", None)
 PORT: int = int(environ.get("PORT", 8080))
 REDIS_URL: str = environ.get("REDIS_URL", "redis://localhost:5432")
 SECRET_KEY: str = environ.get("SECRET_KEY", "asecrettoeverybody")
