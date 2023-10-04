e57bd9c7af20dd5a7a8d43a70f28269d2f861207
Sitt Guruvanich <aekazitt@gmail.com>
2023-10-05 00:00:40 +0700

    Create test for inbound endpoint GET request

---
+++ tests/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.9
+# Copyright (C) 2023 All rights reserved.
+# FILENAME:  ~~/tests/__init__.py
+# VERSION: 	 0.1.0
+# CREATED: 	 2023-10-04 23:52
+# AUTHOR: 	 Sitt Guruvanich <aekazitt+github@gmail.com>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
+
+### Standard Packages ###
+from pytest import fixture
+
+### Third-Party Packages ###
+from fastapi.testclient import TestClient
+
+### Local modules ###
+from serve import app
+
+@fixture
+def test_tesla_ball() -> TestClient:
+    """
+    Sets up a FastAPI TestClient wrapped around Tesla Ball application
+
+    ---
+    :returns: TestClient
+    """
+    return TestClient(app)
---
+++ tests/inbound.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3.9
+# Copyright (C) 2023 All rights reserved.
+# FILENAME:  ~~/tests/inbound.py
+# VERSION: 	 0.1.0
+# CREATED: 	 2023-10-04 23:52
+# AUTHOR: 	 Sitt Guruvanich <aekazitt+github@gmail.com>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
+
+### Third-party packages ###
+from fastapi.responses import Response
+from fastapi.testclient import TestClient
+
+### Local modules ###
+from . import test_tesla_ball
+
+def test_create_inbound_liquidity_request(test_tesla_ball: TestClient):
+    response: Response = test_tesla_ball.get("/inbound")
+    assert response.status_code == 200
+    assert response.json() == {"status": "OK"}
+    print(response.json())
