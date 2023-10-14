656bba48078636325c86aa93422b177fa773777e
Sitt Guruvanich <aekazitt@gmail.com>
2023-10-14 14:31:55 +0700

    Rinse and repeat

--- tests/inbound.py
+++ tests/inbound.py
@@ -16,26 +16,31 @@ from typing import Dict, Generator, Union
 from fastapi.testclient import TestClient
 from httpx import Response
 from orjson import dumps
-from pytest import fixture
+from pytest import fixture, mark
 from tortoise import Tortoise, run_async
 
 ### Local modules ###
 # from src.services.lightning import Lightning
+from src.models import InboundOrder
 from tests import LND_TARGET_HOST, LND_TARGET_PUBKEY, test_tesla_ball
 
 
 ### Module-specific setup-teardown ###
 @fixture(scope="module", autouse=True)
 def setup_teardown() -> Generator:
-    run_async(Tortoise.init(db_url="sqlite://./test.db", modules={"models": ["src.models"]}))
+    run_async(Tortoise.init(db_url="sqlite://./tests/test.db", modules={"models": ["src.models"]}))
     run_async(Tortoise.generate_schemas(True))
+    assert run_async(InboundOrder.all().count()) is None
 
     yield
 
+    run_async(InboundOrder.all().delete())
+    run_async(Tortoise._drop_databases())
     run_async(Tortoise.close_connections())
 
 
-def test_create_inbound_liquidity_request(test_tesla_ball: TestClient) -> None:
+@mark.asyncio
+async def test_01_create_inbound_order(test_tesla_ball: TestClient) -> None:
     body: Dict[str, Union[int, str]] = {
         "feeRate": 3,
         "nodeUri": f"{ LND_TARGET_PUBKEY }@{ LND_TARGET_HOST }:9735",
@@ -43,4 +48,31 @@ def test_create_inbound_liquidity_request(test_tesla_ball: TestClient) -> None:
     }
     response: Response = test_tesla_ball.post("/inbound", content=dumps(body))
     assert response.status_code == 200
-    print(response.json())
+    order: InboundOrder = await InboundOrder.all().order_by("-id").first()
+    assert response.json().get("invoice", None) is not None
+    assert len(response.json().get("invoice", None)) == 379  # regtest invoice length
+    assert response.json().get("invoice", None)[:6] == "lnbcrt"  # lightning bitcoin regtest
+    assert response.json().get("orderId", None) is not None
+    assert response.json().get("orderId", None) == str(order.order_id)
+    
+
+@mark.asyncio
+async def test_02_check_inbound_request(test_tesla_ball: TestClient) -> None:
+    order: InboundOrder = await InboundOrder.all().order_by("-id").first()
+    response: Response = test_tesla_ball.get(f"/inbound?orderId={ order.order_id }")
+    assert response.status_code == 200
+    assert response.json().get("invoice", None) is not None
+    assert isinstance(response.json().get("invoice", None), str)
+    assert len(response.json().get("invoice", None)) == 379
+    assert response.json().get("invoice", None)[:6] == "lnbcrt"  # lightning bitcoin regtest
+    assert response.json().get("invoice", None) == order.invoice
+    assert response.json().get("feeRate", None) is not None
+    assert isinstance(response.json().get("feeRate", None), int)
+    assert response.json().get("feeRate", None) == order.fee_rate
+    assert response.json().get("orderId", None) is not None
+    assert isinstance(response.json().get("orderId", None), str)
+    assert response.json().get("orderId", None) == str(order.order_id)
+    assert len(response.json().get("orderId", None).split("-")) == 5  # dumb test for UUID
+    assert response.json().get("remoteBalance", None) is not None
+    assert isinstance(response.json().get("remoteBalance", None), int)
+    assert response.json().get("remoteBalance", None) == order.remote_balance
