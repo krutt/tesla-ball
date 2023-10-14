36ab2cb7d454cf467baffabb55f74d7c120ad6a2
Sitt Guruvanich <aekazitt@gmail.com>
2023-10-14 17:40:49 +0700

    Rename testsuite to channel_open

--- tests/jobs/channel_open.py
+++ tests/jobs/channel_open.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.9
 # Copyright (C) 2023 All rights reserved.
-# FILENAME:  ~~/tests/jobs/channel.py
+# FILENAME:  ~~/tests/jobs/channel_open.py
 # VERSION: 	 0.1.0
 # CREATED: 	 2023-10-14 15:20
 # AUTHOR: 	 Sitt Guruvanich <aekazitt+github@gmail.com>
