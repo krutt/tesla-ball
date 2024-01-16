05ca1dc983ae1eb16f81bbfea8d985a15be70dbd
Sitt Guruvanich <aekazitt@gmail.com>
2024-01-16 14:42:49 +0700

    Revise FILENAME in topcomment to match filename

--- src/jobs/txn_confirm.py
+++ src/jobs/txn_confirm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.9
 # coding:utf-8
 # Copyright (C) 2023 All rights reserved.
-# FILENAME:    ~~/src/jobs/transact_verify.py
+# FILENAME:    ~~/src/jobs/txn_confirm.py
 # VERSION: 	   0.1.0
 # CREATED: 	   2023-10-16 11:20
 # AUTHOR: 	   Sitt Guruvanich <aekazitt+github@gmail.com>
